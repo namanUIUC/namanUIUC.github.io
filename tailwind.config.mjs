ea33c5d536f0eeb39aefa7041e3a647648c20df9
naman shukla <namanatulshukla@gmail.com>
2025-04-25 15:37:13 -0700

    :sparkles: Add custom button and badge styles to Tailwind configuration and update components for consistent styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -32,5 +32,56 @@ export default {
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function({ addComponents }) {
+      addComponents({
+        '.btn-rounded': {
+          '@apply text-darkText dark:text-text text-sm font-medium inline-flex items-center px-2.5 py-1 rounded-full cursor-pointer transition-colors': {},
+          '@apply bg-primary dark:bg-darkPrimary': {},
+          '@apply border border-primary dark:border-darkPrimary border-opacity-20 dark:border-opacity-20': {},
+          '&:hover': {
+            '@apply bg-opacity-90 dark:bg-opacity-90': {},
+          },
+        },
+        
+        '.badge': {
+          '@apply inline-flex items-center justify-center px-2 py-1 text-xs font-medium rounded-full transition-colors': {},
+          '@apply bg-secondary dark:bg-darkSecondary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-secondary dark:text-darkSecondary': {},
+        },
+        '.badge-primary': {
+          '@apply bg-primary dark:bg-darkPrimary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-primary dark:text-darkPrimary': {},
+        },
+        '.badge-secondary': {
+          '@apply bg-secondary dark:bg-darkSecondary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-secondary dark:text-darkSecondary': {},
+        },
+        '.badge-tertiary': {
+          '@apply bg-tertiary dark:bg-darkTertiary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-tertiary dark:text-darkTertiary': {},
+        },
+        
+        '.btn-flat': {
+          '@apply bg-opacity-10 text-xs font-medium px-2 py-0.5 rounded uppercase transition-colors': {},
+          '@apply hover:bg-opacity-20 dark:hover:bg-opacity-20': {},
+        },
+        '.btn-flat-primary': {
+          '@apply bg-primary dark:bg-darkPrimary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-primary dark:text-darkPrimary': {},
+        },
+        '.btn-flat-secondary': {
+          '@apply bg-secondary dark:bg-darkSecondary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-secondary dark:text-darkSecondary': {},
+        },
+        '.btn-flat-tertiary': {
+          '@apply bg-tertiary dark:bg-darkTertiary bg-opacity-10 dark:bg-opacity-10': {},
+          '@apply text-tertiary dark:text-darkTertiary': {},
+        },
+      })
+    }
+  ],
 };
